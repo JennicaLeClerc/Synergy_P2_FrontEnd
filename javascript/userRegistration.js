f0dc001b5608a6dcb7e839fd2683601b8a83e8f9
Ernst Aine <vilycharles72@gmail.com>
2021-12-08 11:42:14 -0500

    create a Javascript file and validation for user registration

---
+++ javascript/userRegistration.js
@@ -0,0 +1,37 @@
+function joinList(){
+  var nameEl = document.getElementById("firstname");
+  var lname = document.getElementById("lastname");
+  var username = document.getElementById("userName");
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+
+  var msg1 = document.getElementById("msg1");
+  var isValid = true;
+  if(nameEl.value == ""){
+    msg1.innerHTML = "This filed is required";
+
+    isValid = false;
+  }else if(lname.value == ""){
+  msg2.innerHTML = "This filed is required";
+  isValid = false;
+
+  }else if(username.value == ""){
+  msg3.innerHTML = "This filed is required";
+  isValid = false;
+  }else if(email.value == ""){
+  msg4.innerHTML = "This filed is required";
+  isValid = false;
+  }else if(password.value == ""){
+  msg5.innerHTML = "This filed is required";
+  isValid = false;
+  } else{
+    nameEl.nextElementSibling.firstChild.nodeValue = "";
+	lname.nextElementSibling.firstChild.nodeValue = "";
+	username.nextElementSibling.firstChild.nodeValue = "";
+	email.nextElementSibling.firstChild.nodeValue = "";
+	password.nextElementSibling.firstChild.nodeValue = "";
+  }
+
+}
+var mybtn = document.getElementById("join_list");
+mybtn.addEventListener("click", joinList, false);
